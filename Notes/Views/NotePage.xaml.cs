48b6c9dba800edf7e414a4e2a7ecba74ecec8967
HotariTobu <hotari24tools@gmail.com>
2023-11-04 14:22:58 +0900

    first commit

---
+++ Notes/Views/NotePage.xaml.cs
@@ -0,0 +1,58 @@
+namespace Notes.Views;
+
+[QueryProperty(nameof(ItemId), nameof(ItemId))]
+public partial class NotePage : ContentPage
+{
+	public string ItemId
+	{
+		set => LoadNote(value);
+	}
+
+	public NotePage()
+	{
+		InitializeComponent();
+
+		string appDataPath = FileSystem.AppDataDirectory;
+		string randomFilename = $"{Path.GetRandomFileName()}.notes.txt";
+
+		LoadNote(Path.Combine(appDataPath, randomFilename));
+	}
+
+	private async void SaveButton_Clicked(object sender, EventArgs e)
+	{
+		if (BindingContext is Models.Note note)
+		{
+			File.WriteAllText(note.Filename, TextEditor.Text);
+		}
+
+		await Shell.Current.GoToAsync("..");
+	}
+
+	private async void DeleteButton_Clicked(object sender, EventArgs e)
+	{
+		if (BindingContext is Models.Note note)
+		{
+			// Delete the file.
+			if (File.Exists(note.Filename))
+			{
+				File.Delete(note.Filename);
+			}
+		}
+
+		await Shell.Current.GoToAsync("..");
+    }
+
+    private void LoadNote(string filename)
+	{
+		Models.Note note = new Models.Note();
+		note.Filename = filename;
+
+		if (File.Exists(filename))
+		{
+			note.Date = File.GetCreationTime(filename);
+			note.Text = File.ReadAllText(filename);
+		}
+
+		BindingContext = note;
+	}
+}
